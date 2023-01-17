183fee71fb29e37ae3435525554d6e90ba761fec
john <97820338+johm01@users.noreply.github.com>
2023-01-16 22:57:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from settings import * 
 from Enemy import Enemy
 from Tile import Tile
+from level import Level
 
 # Main Player class 
 class Player(pygame.sprite.Sprite):
@@ -15,6 +16,7 @@ class Player(pygame.sprite.Sprite):
         # Player image 
         self.image = pygame.image.load(self.images[0]).convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
+        
         
         self.vel = vel
         self.health = health 
@@ -32,7 +34,7 @@ class Player(pygame.sprite.Sprite):
             self.rect.x -= self.vel
             print('left')
 
-    def run(self):
+    def update(self):
         self.movement(pygame.key.get_pressed())
 
 # Bamboo object class
@@ -41,7 +43,35 @@ class Bamboo(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load(bamboo_img[0])
         self.rect = self.image.get_rect(topleft=pos)
-            
+    
+    def player_on_bamboo(self,player):
+        e = pygame.key.get_pressed()
+        if e[pygame.K_SPACE] and pygame.Rect.colliderect(player.rect,self.rect) and not player.on_bamboo:
+            player.on_bamboo = True
+            print('On Bamboo')
+            # Player is on bamboo
+            if player.on_bamboo:
+                    player.vel = 3
+                    player.moving_x = False
+                    print('On Bamboo', player.on_bamboo)
+
+        # Player moving up and down bamboo
+        if e[pygame.K_UP] and player.on_bamboo:
+                player.rect.y -= player.vel 
+        elif e[pygame.K_DOWN] and player.on_bamboo:
+                player.rect.y += player.vel
+
+        # Jumping from bamboo to bamboo tree
+        if player.on_bamboo and e[pygame.K_SPACE]:
+                pass
+
+        # Jump off bamboo
+        if e[pygame.K_e] and player.on_bamboo:
+                player.on_bamboo = False
+                player.vel = 3
+                player.moving_x = True
+                print('Off Bamboo',player.on_bamboo)
+
 class Game:
     def __init__(self) -> None:
         pygame.init()
@@ -49,71 +79,21 @@ class Game:
         self.sur = pygame.display.get_surface()
         self.clock = pygame.time.Clock()
 
-        # Sprite groups 
-        self.visable_sprite = pygame.sprite.Group()
-        self.obstacle_sprite = pygame.sprite.Group()
-
-        level = level_1
-        
-        for row_index, row in enumerate(level):
-            for col_index,col in enumerate(row):
-                x = row_index * 64
-                y = col_index * 64
-
-                global active_tiles
-                active_tiles = []
-
-                if col == 'x':
-                    Tile(img='./assets/Tiles/tile1.png',pos=(x,y),groups=self.visable_sprite)
-                elif col == 'b':
-                    self.bamboo = Bamboo(pos=(x,y),groups=self.visable_sprite)
-                elif col == 's':
-                    Tile(img='./assets/Tiles/sky.png',pos=(x,y),groups=self.visable_sprite)
-                elif col == 'p':
-                    self.player = Player(vel=3,health=5,groups=self.visable_sprite,pos=(x,y))
+        self.level = Level()
 
     def run(self): 
         # Main game loop 
         while True:
             for event in pygame.event.get():
                if event.type == pygame.QUIT:
-                    pygame.quit()
-
-               # Getting user input  
-               if event.type == pygame.KEYDOWN: 
-                    # Stick to bamboo
-                    if event.key == pygame.K_q and pygame.Rect.colliderect(self.player.rect,self.bamboo.rect) and not self.player.on_bamboo:
-                        self.player.on_bamboo = True
-                        # Player is on bamboo
-                        if self.player.on_bamboo:
-                            self.player.vel = 3
-                            self.player.moving_x = False
-                            print('On Bamboo', self.player.on_bamboo)
-
-                    # Player moving up and down bamboo
-                    if event.key == pygame.K_UP and self.player.on_bamboo:
-                        self.player.rect.y -= self.player.vel 
-                    elif event.key == pygame.K_DOWN and self.player.on_bamboo:
-                        self.player.rect.y += self.player.vel
-
-                    # Jumping from bamboo to bamboo tree
-                    if self.player.on_bamboo and event.key == pygame.K_SPACE:
-                        pass
-
-                    # Jump off bamboo
-                    if event.key == pygame.K_e and self.player.on_bamboo:
-                        self.player.on_bamboo = False
-                        self.player.vel = 3
-                        self.player.moving_x = True
-                        print('Off Bamboo',self.player.on_bamboo)
+                    pygame.quit() 
 
             # Getting Inputs 
-            keys = pygame.key.get_pressed()
-            self.player.run()
+            self.player.update()
 
             # Updating display
             self.screen.fill('black')
-            self.visable_sprite.draw(self.sur)
+            self.level.run()
             pygame.display.update()
             self.clock.tick(FPS)
 
