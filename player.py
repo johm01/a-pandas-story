bc9c0937e7c56da15c68a33a88e516125836f2ce
john <97820338+johm01@users.noreply.github.com>
2023-01-21 22:35:20 -0600

    player jump

--- player.py
+++ player.py
@@ -16,20 +16,41 @@ class Player(pygame.sprite.Sprite):
 
         self.on_bamboo = False
         self.moving_x = True 
+        self.is_jump = False
+        self.jumpCount = 5
 
         # Player gravity
         self.gravity = g
 
+        # Player state 
+        self.state = 'idle'
+
         # Player Movement
     def movement(self,key) -> None:
         if key[pygame.K_RIGHT] and self.moving_x:
+            self.state = 'right'
             self.rect.x += self.vel
-            #self.img = pygame.image.load(self.image[0 + 1])
-            print('right')
         elif key[pygame.K_LEFT] and self.moving_x:
+            self.state = 'left'
             self.rect.x -= self.vel
-            print('left')
+
+        # Player jump
+        if not self.is_jump:
+            if key[pygame.K_SPACE]:
+                self.state = 'jump'
+                self.is_jump = True
+        else:
+            if self.jumpCount >= -10:
+                self.rect.y -= (self.jumpCount * abs(self.jumpCount)) * 0.5
+                self.jumpCount -= 2
+            else:
+                self.state = 'idle'
+                self.jumpCount = 10
+                self.is_jump = False
+                
+        print('State: ',self.state)
 
     def update(self):
         #self.rect.y += self.gravity
         self.movement(pygame.key.get_pressed())
+
