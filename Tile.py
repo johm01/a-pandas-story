dd4a62262edc03c3f4e3f03978398567c2673a77
john <97820338+johm01@users.noreply.github.com>
2023-02-17 17:51:03 -0600

    Update Tile.py

--- Tile.py
+++ Tile.py
@@ -2,7 +2,15 @@ import pygame
 from settings import * 
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self,img,pos,groups):
+    def __init__(self,img,pos,groups,type):
         super().__init__(groups)
         self.image = pygame.image.load(img).convert_alpha()
-        self.rect = self.image.get_rect(topleft=pos)
+        self.rect = self.image.get_rect(topleft=(pos[0],pos[1]-64))
+        self.type = type
+
+    def collectible_collision(self):
+        print('yes')
+
+    def update(self):
+        if self.type == 'coin':
+            self.collectible_collision()
