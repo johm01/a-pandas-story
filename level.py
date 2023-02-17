0212012a2e5471128f1e08d4447acaf68d9f1680
john <97820338+johm01@users.noreply.github.com>
2023-02-17 17:51:22 -0600

    Update level.py

--- level.py
+++ level.py
@@ -25,46 +25,41 @@ class Level:
                 active_tiles = []
 
                 if col == 'x':
-                    Tile(img='./assets/Tiles/tile1.png',pos=(x,y),groups=[self.sprite_group[1],self.sprite_group[2]])
+                    Tile(img='./assets/Tiles/tile1.png',pos=(x,y),groups=[self.sprite_group[1],self.sprite_group[2]],type='tile')
                 
                 if col == 'p':
                     self.player = Player(vel=4,health=5,groups=self.sprite_group[3],pos=(x,y),g=1.2,hp_mod=0)
 
                 if col == 'b':
-                    Tile(img='./assets/Player/bamboo_1.png',groups=[self.sprite_group[0]],pos=(x,y))
+                    Tile(img='./assets/Player/bamboo_1.png',groups=[self.sprite_group[0]],pos=(x,y),type='bamboo')
 
                 if col == 'l':
-                    Tile(img='./assets/Tiles/leaf.png',groups=[self.sprite_group[1]],pos=(x,y))
-
-                if col == 'f':
-                    Tile(img='./assets/Tiles/sky2.png',groups=[self.sprite_group[1]],pos=(x,y)) 
+                    Tile(img='./assets/Tiles/coin.png',groups=[self.sprite_group[1]],pos=(x,y),type='')
 
                 if col == 's':
-                    Tile(img='./assets/Tiles/slab.png',pos=(x,y),groups=[self.sprite_group[1],self.sprite_group[2]])
+                    Tile(img='./assets/Tiles/slab.png',pos=(x,y),groups=[self.sprite_group[1],self.sprite_group[2]],type='tile')
 
                 if col == 't':
-                    Tile(img='./assets/Tiles/spike.png',pos=(x,y),groups=[self.sprite_group[1],self.sprite_group[4]]) 
+                    Tile(img='./assets/Tiles/spike.png',pos=(x,y),groups=[self.sprite_group[1],self.sprite_group[4]],type='trap') 
 
     def trap_collision(self,trap):
         for t in trap:
             if t.rect.colliderect(self.player.rect):
                 # Checking health everytime we hit a trap instead of constantly checking it within the player update method
+                self.player.health -= 1
                 self.player.health_check()
                 if self.player.direction.y > 0: 
                         self.player.rect.bottom = t.rect.top
                         self.player.direction.y = 0
-                        self.player.health -= 1
                 elif self.player.direction.y < 0:
                         self.player.rect.top = t.rect.bottom
                         self.player.direction.y = 0
-                        self.player.health -= 1
 
                 if self.player.direction.x < 0:
                         self.player.rect.left = t.rect.right
-                        self.player.health -= 1
                 elif self.player.direction.x > 0:
                         self.player.rect.right = t.rect.left
-                        self.player.health -= 1
+
     # Player camera
     def camera(self):
         x = self.player.rect.centerx
