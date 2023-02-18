952e3db77bf148ea943ac6e59c02e264a4862af7
john <97820338+johm01@users.noreply.github.com>
2023-02-18 00:06:40 -0600

    Update collectible.py

--- collectible.py
+++ collectible.py
@@ -2,10 +2,12 @@ from settings import *
 import pygame 
 
 class Collectible(pygame.sprite.Sprite):
-    def __init__(self, groups) -> None:
+    def __init__(self, groups, pos, type) -> None:
         super().__init__(groups)
+        self.image = pygame.image.load('').convert_alpha()
+        self.rect = self.image.get_rect(topleft=pos)
         self.sprite_groups = orders
+        self.type = type
 
-    
     def update():
         pass
