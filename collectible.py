d10b1399907ccf95963c6870a7b21bc463663310
john <97820338+johm01@users.noreply.github.com>
2023-02-17 21:32:50 -0600

    Add files via upload

---
+++ collectible.py
@@ -0,0 +1,11 @@
+from settings import *
+import pygame 
+
+class Collectible(pygame.sprite.Sprite):
+    def __init__(self, groups) -> None:
+        super().__init__(groups)
+        self.sprite_groups = orders
+
+    
+    def update():
+        pass
