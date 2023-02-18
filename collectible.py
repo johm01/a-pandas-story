ae1606f78061540c48ce307a07b6d561bd3d35bc
john <97820338+johm01@users.noreply.github.com>
2023-02-18 00:12:52 -0600

    Update collectible.py

--- collectible.py
+++ collectible.py
@@ -4,10 +4,11 @@ import pygame
 class Collectible(pygame.sprite.Sprite):
     def __init__(self, groups, pos, type) -> None:
         super().__init__(groups)
-        self.image = pygame.image.load('').convert_alpha()
+        self.img = './assets/Tiles/coin.png'
+        self.image = pygame.image.load(self.img).convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
         self.sprite_groups = orders
         self.type = type
 
-    def update():
+    def update(self):
         pass
