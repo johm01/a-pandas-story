000b9143d3a8a2c5f45747211a071d2d32a3d06a
john <97820338+johm01@users.noreply.github.com>
2023-02-20 21:30:39 -0600

    Add files via upload

--- Tile.py
+++ Tile.py
@@ -2,8 +2,11 @@ import pygame
 from settings import * 
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self,img,pos,groups,type):
+    def __init__(self,img,pos,groups):
         super().__init__(groups)
         self.image = pygame.image.load(img).convert_alpha()
         self.rect = self.image.get_rect(topleft=(pos[0],pos[1]-64))
-        self.type = type
+
+        self.sprite_group = orders
+
+   
