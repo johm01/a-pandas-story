0953d808dd535b3b7a4def238f53d3f016d36f0c
john <97820338+johm01@users.noreply.github.com>
2023-05-21 00:58:18 -0500

    Add files via upload

---
+++ objects.py
@@ -0,0 +1,44 @@
+import pygame 
+from settings import * 
+
+class Mob(pygame.sprite.Sprite):
+    def __init__(self,img,pos,groups,type):
+        super().__init__(groups)
+        self.is_dead = False
+        self.image = pygame.image.load(img).convert_alpha()
+        self.rect = self.image.get_rect(topleft=(pos[0],pos[1]-64))
+        self.type = type 
+        self.sprite_group = orders
+
+        # If mob is dead 
+        if self.is_dead:
+            for i in range(len(groups)):
+                groups[i].sprite_group.empty()
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self,img,pos,groups):
+        super().__init__(groups)
+        self.replace = False
+        self.image = pygame.image.load(img).convert_alpha()
+        self.rect = self.image.get_rect(topleft=(pos[0],pos[1]-64))
+        self.sprite_group = orders
+
+        if self.replace:
+            self.image = pygame.image.load('./assets/Tiles/dead.png').convert_alpha()
+
+class Trap(pygame.sprite.Sprite):
+    def __init__(self, groups,type,pos) -> None:
+        super().__init__(groups)
+        self.type = type 
+        self.image = pygame.Surface()
+        self.image.fill((0,255,0))
+        self.rect = self.image.get_rect()
+        self.sur = pygame.display.get_surface()
+        self.img = pygame.image.load('').convert_alpha()
+
+        if self.type == 'spike':
+            pass
+    
+    def update(self):
+        pygame.draw.circle(self.image,(255,0,0),(50,50), 5)
+        self.image.blit()
