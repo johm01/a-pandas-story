2e497c05317ec707d3580c248cb2454bc35cca10
john <97820338+johm01@users.noreply.github.com>
2023-05-23 17:56:28 -0500

    Update objects.py

--- objects.py
+++ objects.py
@@ -1,31 +1,5 @@
 import pygame 
 from settings import * 
-
-class Mob(pygame.sprite.Sprite):
-    def __init__(self,img,pos,type,target):
-        super().__init__()
-        self.image = pygame.image.load(img).convert_alpha()
-        self.rect = self.image.get_rect(topleft=(pos[0],pos[1]-64))
-        self.type = type 
-        self.target = target
-        self.sprite_group = s_groups
-
-        self.direction = pygame.math.Vector2(0,0)
-        self.vel = 0.3
-        self.gravity = 9.8
-        self.is_dead = False
-        self.is_falling = True
-        self.is_ground = False
-
-    def mob_collision(self):
-        for t in self.type:
-            if t.rect.colliderect(self.target.rect) and self.type == self.sprite_group["mob_1"]:
-                print('yes')
-    
-    def update(self):
-        collision_h(self,self.sprite_group["collide"])
-        collision_v(self,self.sprite_group["collide"])
-        self.mob_collision()
         
 class Tile(pygame.sprite.Sprite):
     def __init__(self,img,pos,groups):
